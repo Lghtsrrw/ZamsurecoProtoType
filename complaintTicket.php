0bb29abbc0d9a80e80925c7ee1578b3d4d2c3494
Syed Ganih <msprouse843@gmail.com>
2020-08-12 15:34:11 +0800

    partially functional complaint-ticket

--- complaintTicket.php
+++ complaintTicket.php
@@ -14,43 +14,53 @@
     <link rel="stylesheet" type ="text/css"href="stylesheets/webStyle.css">
     <script src="js/jquery-3.5.1.min.js"></script>
     <script type="text/javascript">
-    $(document).ready(function(){
+      $(document).ready(function(){
         $("#btnBack").click(function(){
           window.location.href = "signin.php";
         });
 
-        const url = 'json/philippine_provinces_cities_municipalities_and_barangays_2019v2.json';
         let dropdown = $('#ddRegion');
         dropdown.empty();
         dropdown.append('<option selected="true" disabled>Choose Region</option>');
         dropdown.prop('selectedIndex', 0);
+        <?php populateRegion(); ?>
 
-        //populate Region dropdown
-        $.getJSON(url, function(data){
-            $.each(data, function ( entry) {
-                dropdown.append($('<option></option>').text(entry));
-            })
+        $('#ddRegion').change(function(){
+          if($('#ddRegion').val() !== 'Choose Region'){
+            $('#divProvince').show();
+
+            let ddProvince = $('#ddProvince');
+            ddProvince.empty();
+            ddProvince.append('<option selected = "true" disabled>Choose Province</option>');
+            ddProvince.prop('selectedIndex',0);
+            var _reg = $('#ddRegion').val();
+            <?php populateProvince('REGION XI (DAVAO REGION)'); ?>
+          }
         });
 
-        //display Province ComboBox and populate
-        $("#ddRegion").change(function() {
-            if($("#ddRegion").val() != "Choose Region"){
-                $("#divProvince").show();
+        $('#ddProvince').change(function(){
+          if($('#ddProvince').val() !== 'Choose Province'){
+            $('#divMunicipal').show();
 
-                let ddProvince = $('#ddProvince');
-                ddProvince.empty();
-                ddProvince.append('<option selected="true" disabled>Choose Province</option>');
-                ddProvince.prop('selectedIndex', 0);
-
-                if($('#ddRegion').val() == "01"){
-                  $.getJSON(url, function(data){
-                      $.each(data, function (key, region) {
-                          ddProvince.append($('<option></option>').text(key));
-                      })
-                  });
-                }
-            }
+            let ddMunicipal = $('#ddMunicipal');
+            ddMunicipal.empty();
+            ddMunicipal.append('<option selected = "true" disabled>Choose City/Municipal</option>');
+            ddMunicipal.prop('selectedIndex', 0);
+            <?php populateMunicipal('ZAMBOANGA DEL SUR'); ?>
+          }
         });
+
+      $('#ddMunicipal').change(function(){
+        if($('#ddMunicipal').val() !== "Choose City/Municipal"){
+          $('#divBrgy').show();
+
+          let ddBrgy = $('#ddBrgy');
+          ddBrgy.empty();
+          ddBrgy.append('<option selected = "true" disabled>Choose Barangay</option>');
+          ddBrgy.prop('selectedIndex', 0);
+          <?php populateBrgy('PAGADIAN CITY (Capital)') ?>
+        }
+      });
     });
     </script>
     <title>Create a Ticket</title>
@@ -61,12 +71,10 @@
         <li class = "liBack">
             <a id="btnBack">Back</a>
         </li>
-
         <li class = "liLogout">
             <a id="btnLogout" href="index.php?logout='1'" style='color:red;'>Logout</a>
         </li>
     </ul>
-
     </div>
     <div class = "ticket" style="width:30%; text-align:left; padding:10px; margin:auto;border: 3px solid rgb(0, 66, 128);">
         <h1>Create a Ticket</h1>
@@ -84,20 +92,21 @@
 
             <label for="lblRegion">Region</label><br>
             <select name="region" id="ddRegion" style="width:100%; height:30px; text-align:CENTER;">
+              <?php populateRegion(); ?>
             </select><br>
             <div id = "divProvince" style = "display: none">
                 <label for="lblProvince">Province</label><br>
                 <select name="province" id="ddProvince" style="width:100%; height:30px; text-align:CENTER;">
-                </select><br><br>
+                </select>
             </div><br>
             <div id = "divMunicipal" style = "display:none">
                 <label for="lblMunicipal">Municipal</label><br>
-                <select name="municipal" id="ddBrownOut" style="width:180px">
+                <select name="municipal" id="ddMunicipal" style="width:100%; height:30px; text-align:CENTER;">
                 </select>
             </div><br>
             <div id = "divBrgy" style = "display:none">
                 <label for="lblBrgy">Barangay</label><br>
-                <select name="brgy" id="ddBrgy" style="width:180px">
+                <select name="brgy" id="ddBrgy" style="width:100%; height:30px; text-align:CENTER;">
                 </select>
             </div><br>
             <div id="divPurok" style = "display:none">
