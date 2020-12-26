64bbbeafe944809089215a7002ccaffcc570eb56
Thales <msprouse843@gmail.com>
2020-12-26 13:52:40 +0800

    deleted adyen module

---
+++ userauthenticate.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-ico" href="img/favicon.ico"/>
+    <link href="stylesheets/allStyle.css" rel="stylesheet" type="text/css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <title>ZAMSURECO I - Sign In</title>
+
+    <div class="divHeader">
+      <h3 style="margin-bottom: 0">Z A M S U R E C O - I</h3>
+      <p style="font-size: 1.4vh; margin-top:0">Governor Vicente Cerilles St., San Francisco Dist, Pagadian City, Zamboanga del Sur</p>
+      <div class="buttons" style="width:100%; text-align: left; padding: 1%; ">
+        <button id = "btnLogin" class="mainBtn" style="width:auto">LOGIN</button>
+        <button id = "btnRegister" class="mainBtn" style="width:auto">REGISTER</button>
+        <button id = "btnTrack" class="mainBtn" style="width:auto">TRACK COMPLAINT</button>
+        <button id = "btnGuest" class="mainBtn" style="width:auto; background-color:#f1964c; ">LOGIN AS GUEST</button>
+      </div>
+    </div>
+  </head>
+
+  <body style="
+  background: url('img/payment.png') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;">
+    <divstyle="border: 1px solid black; width:80%; height: 100px;">
+      <p>Lorem ipsum</p>
+    </div>
+  </body>
+
+  <footer style="">
+    <div class="divFooter">
+      <p>Zamsureco-I Mobile App | All Rights Reserved</p>
+      <p>2020</p>
+    </div>
+  </footer>
+</html>
